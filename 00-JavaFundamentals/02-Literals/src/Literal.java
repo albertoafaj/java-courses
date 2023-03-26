70e7d7a0d0c658b40aec3a124909d4e085e2b254
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-26 09:50:19 -0300

    feat: exemplify literals in java fundamentals

---
+++ 00-JavaFundamentals/02-Literals/src/Literal.java
@@ -0,0 +1,21 @@
+public class Literal {
+
+	public static void main(String[] args) {
+		int numero;
+		String cadeiaCaracteres;
+		char caracter;
+		boolean certoOuErrado;
+
+		numero = 20;
+		cadeiaCaracteres = "** Meu Curso de Java Fundamentos";
+		caracter = 'N';
+		certoOuErrado = false;
+
+		System.out.println(" O valor de n�mero � : " + numero);
+		System.out.println(" O valor de cadeiaCaracteres � : " + cadeiaCaracteres);
+		System.out.println(" O valor de caracter � : " + caracter);
+		System.out.println(" O valor de certoOuErrado � : " + certoOuErrado);
+
+	}
+
+}
