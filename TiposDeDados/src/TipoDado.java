c43a5026bb7bf019172cad84087bc0443c215028
Alberto Araújo <albertoafaj@hotmail.com>
2023-03-26 09:36:26 -0300

    feat: exemplify data types in java

---
+++ TiposDeDados/src/TipoDado.java
@@ -0,0 +1,19 @@
+
+public class TipoDado {
+
+	public static void main(String[] args) {
+
+		int numero;
+		float decimalCurto;
+		double decimalLongo;
+		char caracter;
+		String cadeiaCaracteres;
+
+		numero = 10;
+		decimalCurto = 10.5f;
+		decimalLongo = 20.5;
+		caracter = 'A';
+		cadeiaCaracteres = "** Meu Curso de Java Fundamentos em 2023 **";
+	}
+
+}
